e15195ec26bd3501557a5864b37ccc2533edd0c3
MuhammadMullah <muhammadmullah16@gmail.com>
2019-08-27 15:50:44 +0300

    finishing on jsx

--- jsx/src/index.js
+++ jsx/src/index.js
@@ -1,20 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-function getButtonText() {
-    return 'Click On Me'
-}
+// function getButtonText() {
+//     return 'Click On Me'
+// }
 
 const App = () => {
-    // const buttonText = 'Click Me'
+    const buttonText = 'Click Me'
+    const labelText = 'Enter Name:'
     return (
         <div>
-            <label className="label" for="name">
-                Enter Name:
+            <label className="label" htmlFor="name">
+                {labelText}
             </label>
             <input id="name" type="text" />
             <button style={{backgroundColor: 'blue', color: 'white'}}>
-                {getButtonText()}
+                {buttonText}
             </button>
         </div>
     )
