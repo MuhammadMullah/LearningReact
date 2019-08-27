9dc5a4572e1ca6f0e00ae99b778475ba92939fa3
MuhammadMullah <muhammadmullah16@gmail.com>
2019-08-27 19:23:42 +0300

    done with ApprovalCard, learned on props basically

--- components/src/index.js
+++ components/src/index.js
@@ -1,12 +1,39 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import faker from 'faker';
+import CommentDetail from './CommentDetail'
+import ApprovalCard from './ApprovalCard'
 
 
 const App = () => {
     return (
-        <div>
-            Hi there
+        <div className="ui container comments">
+            <ApprovalCard>
+                <CommentDetail
+                author= "Mullah" 
+                timeAgo="Today at 8:00PM" 
+                content="Nice Blog Post!"
+                avatar={faker.image.avatar()}
+                />
+            </ApprovalCard>
+            
+            <ApprovalCard>
+                <CommentDetail 
+                author= "Issa" 
+                timeAgo="Today at 3:45AM" 
+                content="I Like the subject"
+                avatar={faker.image.avatar()}
+                />
+            </ApprovalCard>
+
+            <ApprovalCard>
+                <CommentDetail 
+                author= "Hassan" 
+                timeAgo="Yesterday at 4:00AM" 
+                content="I love writing"
+                avatar={faker.image.avatar()}
+                />
+            </ApprovalCard>
         </div>
     )
 }
